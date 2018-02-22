d75bbec7bfd6cc4adb5ec4556a03321756b0d029
Digital Alchemist <developer.anwar@gmail.com>
2018-02-21 21:54:35 -0500

    Project Setup and Configurations

---
+++ src/test/java/com/dc/automation/automatedtestingspring/AutomatedTestingSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dc.automation.automatedtestingspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutomatedTestingSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
